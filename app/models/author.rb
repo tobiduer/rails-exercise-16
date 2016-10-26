ca49f4a0976f9f3454717f0fdac65057c751c002
Tobias Dürschmid <tobias.duerschmid@student.hpi.de>
2016-10-26 20:44:16 +0000

    Add name method for author. Close #10

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
-class Author < ActiveRecord::Base
+class Author < ActiveRecord::Base 
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :homepage, presence: true
+
+	def name
+		first_name + ' ' + last_name
+	end
 end
