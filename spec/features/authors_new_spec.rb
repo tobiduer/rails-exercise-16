db4473e60b9f1c3f1c82f728618e311915aa018f
Tobias Dürschmid <tobias.duerschmid@student.hpi.de>
2016-10-26 19:03:32 +0000

    Failing test for #4

--- spec/features/authors_new_spec.rb
+++ spec/features/authors_new_spec.rb
@@ -7,4 +7,9 @@ describe "Authors new page", :type => :feature do
     expect(page).to have_field("First name")
   end
 
+  it "should have the field 'last name'" do
+	visit "/authors/new"
+    expect(page).to have_field("Last name")
+  end
+
 end
