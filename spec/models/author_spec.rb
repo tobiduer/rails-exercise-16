4a2dc97b0128032c911d128a38cc88884cbb5549
Tobias Dürschmid <tobias.duerschmid@student.hpi.de>
2016-10-26 20:43:47 +0000

    Failing test for #10

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Author, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid" do
+    # Using the shortened version of FactoryGirl syntax.
+    # Add:  "config.include FactoryGirl::Syntax::Methods" (no quotes) to your spec_helper.rb
+    expect(build(:author)).to be_valid
+  end
+
+  it "has a name" do
+    # Using the shortened version of FactoryGirl syntax.
+    # Add:  "config.include FactoryGirl::Syntax::Methods" (no quotes) to your spec_helper.rb
+    expect(build(:author)).to respond_to(:name)
+  end
+
+  it "has a correct name" do
+    # Using the shortened version of FactoryGirl syntax.
+    # Add:  "config.include FactoryGirl::Syntax::Methods" (no quotes) to your spec_helper.rb
+    expect(build(:author).name).to eq("Alan Turing")
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -39,6 +40,7 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
+
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
