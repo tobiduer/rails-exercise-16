2a074ea6482acb9e9ea5b8ff093622f529198acf
Tobias Dürschmid <tobias.duerschmid@student.hpi.de>
2016-10-26 23:01:06 +0000

    Failing test #14

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
-describe "Authors page", :type => :feature do
+describe "Authors index page", :type => :feature do
   
+  before(:context) do
+    @author = build(:author)
+    @author.save
+  end
+
   it "should render" do
 	visit "/authors/"
     expect(page).to have_text("Authors")
   end
 
+  it "should list name and homepage of all authors" do
+	visit "/authors/"
+    expect(page).to have_text(@author.name)
+    expect(page).to have_text(@author.homepage)
+  end
 end
